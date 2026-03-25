908d71cf9c22ba17ac8d651e670a260db77663ec
datajav <156545837+datajav@users.noreply.github.com>
2026-03-24 21:14:20 -0500

    Create chunker.py

---
+++ ingestion/chunker.py
@@ -0,0 +1,10 @@
+import re 
+from typing import Any
+
+SECTION_PATTERNS = [
+
+    re.compile(r"^(\d+\.(?:d+\.?)*)\s+([A-Z][^\n]{0,80}})", re.multiline), 
+
+    re.compile(r"^(Article|Section|ARTICLE|SECTION)\s+(\d+\.?\d*\.?\d*)\s*[:\--]?\s*([A-Z][^\n]{0,80})?", re.MULTILINE), 
+    
+]
